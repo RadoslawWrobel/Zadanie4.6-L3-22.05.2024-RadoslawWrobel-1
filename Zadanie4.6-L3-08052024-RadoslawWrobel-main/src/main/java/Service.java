3ebc1871f86d978be32213f5b963ab47aaed1258
Radosław Wróbel <161605563+RadoslawWrobel@users.noreply.github.com>
2024-05-22 12:45:19 +0000

    Dodanie wyszukiwania po imieniu

---
+++ Zadanie4.6-L3-08052024-RadoslawWrobel-main/src/main/java/Service.java
@@ -0,0 +1,47 @@
+import java.util.Collection;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Service {
+
+  public void addStudent(Student student) throws IOException {
+    var f = new FileWriter("db.txt", true);
+    var b = new BufferedWriter(f);
+    b.append(student.ToString());
+    b.newLine();
+    b.close();
+  }
+
+  public Collection<Student> getStudents() throws IOException {
+    var ret = new ArrayList<Student>();
+    var f = new FileReader("db.txt");
+    var reader = new BufferedReader(f);
+    String line = "";
+    while ((line = reader.readLine()) != null) {
+      ret.add(Student.Parse(line));
+    }
+    reader.close();
+    return ret;
+  }
+
+  public void clearDatabase() throws IOException {
+    var f = new FileWriter("db.txt", false);
+    var b = new BufferedWriter(f);
+    b.write("");
+    b.close();
+  }
+
+  public Student findStudentByName(String name) throws IOException {
+    var students = getStudents();
+    for (Student student : students) {
+      if (student.GetName().equalsIgnoreCase(name)) {
+        return student;
+      }
+    }
+    return null;
+  }
+}
--- Zadanie4.6-L3-08052024-RadoslawWrobel-main/src/main/java/Student.java
+++ Zadanie4.6-L3-08052024-RadoslawWrobel-main/src/main/java/Student.java
@@ -1,9 +1,11 @@
 public class Student {
+
   private String Name;
   private String LastName;
   private int AlbumNumber;
   private int Age;
-  private String DataUrodzenia;
+  private String DataUrodzenia; 
+
   public Student(String name, String lastName, int albumNumber, int age, String dataurodzenia) {
     Name = name;
     LastName = lastName;
@@ -11,17 +13,20 @@ public class Student {
     Age = age;
     DataUrodzenia = dataurodzenia;
   }
-  public String GetName() {return Name;}
+
+  public String GetName() { return Name; }
   public String GetLastName() { return LastName; }
   public int GetAlbumNumber() { return AlbumNumber; }
-  public int GetAge() {return Age;}
-  public String GetDataUrodzenia() {return DataUrodzenia;}
+  public int GetAge() { return Age; }
+  public String GetDataUrodzenia() { return DataUrodzenia; }
+
   public String ToString() {
-    return Name + " " + LastName + " " + Integer.toString(AlbumNumber) + " " + Integer.toString(Age) + " " + DataUrodzenia;
+    return Name + " " + LastName + " " + AlbumNumber + " " + Age + " " + DataUrodzenia;
   }
+
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 5)
+    if (data.length != 5)
       return new Student("Parse Error", "Parse Error", -1, -1, "00.00.0000");
     return new Student(data[0], data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]), data[4]);
   }
